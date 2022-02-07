6333414ca57b1ee6f1c5759e8ab4894e388726cd
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-07 13:42:23 +0600

    DOM JS file added

---
+++ dom-js/dom1.js
@@ -0,0 +1,7 @@
+const blogTiltes = document.getElementById('first-title');
+blogTiltes.style.color = 'green';
+blogTiltes.style.textAlign = 'center';
+
+const blogTexts = document.getElementById('first-text');
+blogTexts.style.border = ' dotted 2px';
+blogTexts.style.padding = '20px';
